4d96931bb62a081e7ae0d698ba1a572820aa0661
Emmanuel F. Werr <47487994+emmanuelfwerr@users.noreply.github.com>
2022-01-24 00:17:06 +0100

    Removing password from db connection

--- landing/export_to_formatted.py
+++ landing/export_to_formatted.py
@@ -119,7 +119,7 @@ weather_df
 from sqlalchemy import create_engine
 
 # Create an engine instance
-conn_string = 'postgresql://postgres:ETS80321123GOM1!@localhost:5432/formatted_zone'
+conn_string = 'postgresql://postgres:****************@localhost:5432/formatted_zone'
 db = create_engine(conn_string)
 
 # Connect to PostgreSQL server
